a0bf5165adf7071e2121b53400f2de8a12248762
Artem ST <artem.stepanenko.kit@gmail.com>
2026-01-12 10:33:11 +0200

    feat: Enhance authentication and routing with JWT and client identification middleware

--- src/ProjectS.Gateway/Program.cs
+++ src/ProjectS.Gateway/Program.cs
@@ -1,16 +1,29 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthentication()
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer("Bearer", options =>
     {
-        options.Authority = builder.Configuration["USERS_API_URL"];
-        options.RequireHttpsMetadata = false;
-        options.TokenValidationParameters = new()
+        var tokenKey = builder.Configuration["TokenKey"]
+            ?? throw new ArgumentException("TokenKey is not configured");
+
+        options.TokenValidationParameters = new TokenValidationParameters
         {
-            ValidateAudience = false
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(tokenKey)
+            ),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero
         };
     });
 
@@ -21,8 +34,31 @@ var app = builder.Build();
 
 app.UseRouting();
 
-//app.UseAuthentication();
-//app.UseAuthorization();
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Middleware to add client identifier for rate limiting
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+app.Use(async (context, next) =>
+{
+    if (!(context.User?.Identity?.IsAuthenticated ?? false))
+    {
+        // Try forwarded IP first
+        var ip =
+            context.Request.Headers["X-Forwarded-For"].FirstOrDefault()
+            ?? context.Connection.RemoteIpAddress?.ToString();
+
+        if (!string.IsNullOrWhiteSpace(ip))
+        {
+            context.Request.Headers["X-Client-Id"] = ip;
+        }
+    }
+
+    await next();
+});
 
 await app.UseOcelot();
 
